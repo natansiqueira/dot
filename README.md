07437e4c9b1edc187ed9c331408e194327590375
Natan Siqueira Alves <natan.siqueira@icloud.com>
2023-08-08 22:58:27 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# dot
+
+## let's go
+**1.** Check for software updates.
+```sh
+$ sudo softwareupdate -i -r
+```
+
+**2.** Install Xcode Command Line Tools.
+```sh
+$ xcode-select --install
+```
+
+**3.** Install it all.
+> don't just like _run_ this, [check the file contents!](bootstrap.sh).
+
+```sh
+$ curl -L https://raw.github.com/natanounatan/dot/master/bootstrap.sh | sh
+```
---
+++ bootstrap.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+# --------------–-------–--------–--------–-------–--–-----
+# Prerequisites
+# --------------–-------–--------–--------–-------–--–-----
+sudo -v # Ask for admin upfront
+
+if test ! $(which brew)
+then
+  echo " → Installing Homebrew for package management..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/natanounatan/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+brew update
+brew upgrade
+
+# echo "→ Downloading dotfiles into user folder..."
+# git clone https://github.com/natanounatan/dot.git ~/dot
+cd ~/dot
+# echo "✓ dotfiles download OK."
+
+# --------------–-------–--------–--------–-------–--–-----
+# GUI apps
+# --------------–-------–--------–--------–-------–--–-----
+echo "→ Installing GUI apps..."
+brew install --cask $(cat brew/caskfile|grep -v "#")
+
+# --------------–-------–--------–--------–-------–--–-----
+# CLI apps
+# --------------–-------–--------–--------–-------–--–-----
+echo "→ Installing CLI apps..."
+brew install $(cat brew/brewfile|grep -v "#")
+
+echo "→ Configuring Git..."
+ln -sf ~/dot/git/config ~/.gitconfig
+ln -sf ~/dot/git/ignore_global ~/.gitignore_global
+# Add personal information in your .gitlocal, not .gitconfig!
+
+echo "→ Configuring ssh..."
+if [ ! -d ~/.ssh ]
+then
+    ssh-keygen -t ed25519 -C "natan.siqueira@icloud.com" -f ~/.ssh/id_personal
+fi
+ln -sf ~/dot/git/ssh-config ~/.ssh/config
+
+# --------------–-------–--------–--------–-------–--–-----
+# macOS settings
+# --------------–-------–--------–--------–-------–--–-----
+echo "→ Setting macOS defaults... (It'll shut down Terminal, bye!)"
+sh misc/macos
+
+echo "✓ That's all folks."
